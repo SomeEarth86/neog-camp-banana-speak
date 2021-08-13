648770105e969946dbcf7c9394b597b66e158e3e
Samarth Krishna Joshi <skrishnaj86@gmail.com>
2021-08-13 20:26:59 +0530

    refactor

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
-var outputDiv = document.querySelector("#output");
+var  outputDiv = document.querySelector("#output");
 
 // var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
 
-var serverURL = "	https://api.funtranslations.com/translate/minion.json"
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
 function getTranslationURL(input){
     return serverURL + "?" +"text=" + input
@@ -23,8 +23,8 @@ function clickHandler(){
    fetch(getTranslationURL(inputTxt))
    .then( response => response.json())
    .then(json => {
-    var translateText =  json.contents.translated;
-    outputDiv.innerText = translateText;
+    var translatedText =  json.content.translated;
+    outputDiv.innerText = translatedText;
    })
    .catch(errorHandler)
 };
